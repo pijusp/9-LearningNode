3908cd85c1ffa38e5fd98cde8cb2289ef5e0d673
Pijus <p.pozera@gmail.com>
2023-01-22 15:25:33 +0200

    Npm modules

--- index.js
+++ index.js
@@ -1,38 +1,6 @@
-const fsPromises = require('fs').promises;
-const path = require('path');
+const { format } = require('date-fns');
+const { v4: uuid } = require('uuid');
 
-const fileOps = async () => {
-  try {
-    const data = await fsPromises.readFile(path.join(__dirname, 'files', 'starter.txt'), 'utf8');
-    console.log(data);
-    await fsPromises.unlink(path.join(__dirname, 'files', 'starter.txt'));
+console.log(format(new Date(), 'yyyyMMdd\tHH:mm:ss'));
 
-    await fsPromises.writeFile(path.join(__dirname, 'files', 'promiseWrite.txt'), data);
-    await fsPromises.appendFile(path.join(__dirname, 'files', 'promiseWrite.txt'), '\n\nNice to meet you');
-    await fsPromises.rename(path.join(__dirname, 'files', 'promiseWrite.txt'), path.join(__dirname, 'files', 'promiseComplete.txt'));
-    const newData = await fsPromises.readFile(path.join(__dirname, 'files', 'promiseComplete.txt'), 'utf8');
-    console.log(newData);
-  } catch (err) {
-    console.error(err);
-  }
-};
-fileOps();
-// fs.writeFile(path.join(__dirname, 'files', 'reply.txt'), 'Nice to meet you', (err) => {
-//   if (err) throw err;
-//   console.log('Write complete');
-
-//     fs.appendFile(path.join(__dirname, 'files', 'reply.txt'), '\n\nYes it is', (err) => {
-//       if (err) throw err;
-//       console.log('Append complete');
-
-//         fs.rename(path.join(__dirname, 'files', 'reply.txt'), path.join(__dirname, 'files', 'newReply.txt'), (err) => {
-//             if (err) throw err;
-//             console.log('Rename complete');
-//         });
-//     });
-// });
-
-process.on('uncaughtException', err => {
-  console.error(`There was an uncaught error: ${err}`);
-  process.exit(1);
-});
+console.log(uuid());
--- server.js
+++
@@ -1,6 +0,0 @@
-const { add, subtract, divide, multiply } = require('./math');
-
-console.log(add(2, 3));
-console.log(subtract(2, 3));
-console.log(multiply(2, 3));
-console.log(divide(2, 3));
